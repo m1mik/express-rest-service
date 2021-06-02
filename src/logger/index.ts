99030e878d1309ebfa94f1445f1b8a9b40bb793c
Mikhail Shumski <m1mik@tut.by>
2021-06-02 19:38:57 +0300

    feat: winston included

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,16 +1,30 @@
 import { NextFunction, Request, Response } from 'express';
+import * as winLog from 'winston';
 
+type Winston = {
+  log: (description: { level: string; message: string }) => void;
+};
 class Logger {
   private request: Request;
 
-  constructor(req: Request) {
+  private winston: Winston;
+
+  constructor(req: Request, winstonInstance: Winston) {
     this.request = req;
+    this.winston = winstonInstance;
   }
 
   public logUrl = (): void =>
-    console.log(`url: ${this.request.baseUrl}${this.request.path}`);
+    this.winston.log({
+      level: 'info',
+      message: `url: ${this.request.baseUrl}${this.request.path}.`,
+    });
 
-  public logMethod = (): void => console.log(`method: ${this.request.method}`);
+  public logMethod = (): void =>
+    this.winston.log({
+      level: 'info',
+      message: `method: ${this.request.method}.`,
+    });
 
   public logParams = (): void => {
     let finalString = '';
@@ -18,9 +32,15 @@ class Logger {
       for (const [param, value] of Object.entries(this.request.params)) {
         finalString += `${param}: ${value}`;
       }
-      console.log(`params: ${finalString}`);
+      this.winston.log({
+        message: `params: ${finalString}.`,
+        level: 'info',
+      });
     } else {
-      console.log(`Request has no params.`);
+      this.winston.log({
+        message: `Request has no params.`,
+        level: 'info',
+      });
     }
   };
 
@@ -30,26 +50,57 @@ class Logger {
       for (const [param, value] of Object.entries(this.request.query)) {
         finalString += `${param}: ${value}`;
       }
-      console.log(`query params: ${finalString}`);
+      this.winston.log({
+        message: `query params: ${finalString}.`,
+        level: 'info',
+      });
     } else {
-      console.log(`Request has no query params`);
+      this.winston.log({
+        message: `Request has no query params.`,
+        level: 'info',
+      });
     }
   };
 
-  public logBody = (): void => console.log(this.request.body);
+  public logBody = (): void =>
+    this.winston.log({
+      message: `${this.request.body}.`,
+      level: 'info',
+    });
+
+  public separate = (): void =>
+    this.winston.log({ level: 'info', message: '-------------------------' });
+
+  public showDate = (): void =>
+    this.winston.log({ level: 'info', message: `${new Date().toISOString()}` });
 }
+
+export const winstonInstance: Winston = winLog.createLogger({
+  level: 'info',
+  format: winLog.format.simple(),
+  transports: [
+    new winLog.transports.File({ filename: 'errors.log', level: 'error' }),
+    new winLog.transports.File({
+      filename: 'exceptions.log',
+      level: 'emerg',
+    }),
+    new winLog.transports.File({ filename: 'main.log', level: 'info' }),
+    new winLog.transports.Console(),
+  ],
+});
 
 const loggerActor = (
   req: Request,
   _res: Response,
   next: NextFunction
 ): void => {
-  const logger: Logger = new Logger(req);
+  const logger: Logger = new Logger(req, winstonInstance);
+  logger.showDate();
   logger.logUrl();
   logger.logMethod();
   logger.logParams();
   logger.logQueryParams();
-  console.log('-----------------------------');
+  logger.separate();
   next();
 };
 
