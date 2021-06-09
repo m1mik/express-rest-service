97065898d4d8dc4148f650243818ff5ca3166c76
Mikhail Shumski <m1mik@tut.by>
2021-06-09 18:02:09 +0300

    feat: minimal requirements done

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
+// import { Sequelize } from 'sequelize';
 import { PORT } from './common/config';
 
 import app from './app';
 
-app.listen(PORT, () => {
+// const sequelize = new Sequelize(
+//   'postgres://myuser:password@172.19.0.2:5433/hello_db'
+// );
+
+app.listen(PORT, async () => {
   console.log(`App is running on http://localhost:${PORT}`);
+  // try {
+  //   await sequelize.authenticate();
+  //   console.log('Connection has been established successfully.');
+  // } catch (error) {
+  //   console.error('Unable to connect to the database:', error);
+  // }
+
   // throw new Error('uncaughtException fired!');
   // Promise.reject(new Error('rejecter'));
 });
